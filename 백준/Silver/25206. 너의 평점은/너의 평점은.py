b11f273406fbc771663c14a14bd9b41fb1b62c5a
Youjin <114130942+Youjiiin@users.noreply.github.com>
2024-01-09 17:18:23 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,14 @@
+rating = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
+grade = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
+
+sum = 0.0
+total = 0.0
+
+for _ in range(20):
+    a, b, c = input().split()
+    b = float(b)
+    if c != "P":
+        total += b
+        sum += b * grade[rating.index(c)]
+
+print(sum / total)
