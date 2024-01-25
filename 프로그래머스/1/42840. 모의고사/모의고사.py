f7dfc7b14fe5a2ee525e993617e006ebe059591c
Youjin <114130942+Youjiiin@users.noreply.github.com>
2024-01-26 01:29:10 +0900

    [level 1] Title: 모의고사, Time: 3.46 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,20 @@
+def solution(answers):
+    answer = []
+    p1 = [1, 2, 3, 4, 5]
+    p2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0, 0, 0]
+
+    for i in range(len(answers)):
+        if answers[i] == p1[i % len(p1)]:
+            score[0] += 1
+        if answers[i] == p2[i % len(p2)]:
+            score[1] += 1
+        if answers[i] == p3[i % len(p3)]:
+            score[2] += 1
+
+    for i in range(len(score)):
+        if max(score) == score[i]:
+            answer.append(i + 1)
+    answer.sort()
+    return answer
