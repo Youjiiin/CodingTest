c4c8cad1d626eba86465da04e75ac570b1efd073
Youjin <114130942+Youjiiin@users.noreply.github.com>
2024-01-25 17:14:31 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,23 @@
+def solution(n, lost, reserve):
+    # 잃어버린 학생 - 여벌 옷을 가지고 있는 학생이 바로 옆 번호인지 확인
+    # [2, 4] [3]인 경우, 한 명은 빌려주고 한 명은 못 빌려줌
+    reserve.sort()
+    lost.sort()
+    # 잃어버렸는데 여벌이 있는 경우
+    for i in reserve[:]:
+        if i in lost:
+            reserve.remove(i)
+            lost.remove(i)
+
+    # 옆 사람이 여벌있는지 확인
+    #for i in lost:
+    #    if i - 1 in reserve or i + 1 in reserve:
+    #        lost.remove(i)
+            
+    for i in reserve:
+        if i-1 in lost:
+            lost.remove(i-1)
+        elif i+1 in lost:
+            lost.remove(i+1)
+
+    return n - len(lost)
