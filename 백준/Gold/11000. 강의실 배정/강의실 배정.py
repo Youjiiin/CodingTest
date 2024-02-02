16ee0cbd56c8fa7a7eec47c3d38457c351fe18cf
Youjin <114130942+Youjiiin@users.noreply.github.com>
2024-02-02 20:56:37 +0900

    [Gold V] Title: 강의실 배정, Time: 404 ms, Memory: 73448 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,19 @@
+import heapq, sys
+input = sys.stdin.readline
+n = int(input())
+
+time = []
+for _ in range(n):
+    time.append(list(map(int, input().split())))
+
+time.sort()
+room = []
+heapq.heappush(room, time[0][1])
+
+for i in range(1, n):
+    if time[i][0] < room[0]:
+        heapq.heappush(room, time[i][1])
+    else:
+        heapq.heappop(room)
+        heapq.heappush(room, time[i][1])
+print(len(room))
