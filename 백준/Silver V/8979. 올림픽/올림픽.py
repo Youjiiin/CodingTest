4f4e7e1df476342640ed744e3981b03a8c2f73f0
Youjin <114130942+Youjiiin@users.noreply.github.com>
2024-02-15 19:29:27 +0900

    [Silver V] Title: 올림픽, Time: 124 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver V/8979. 올림픽/올림픽.py
+++ 백준/Silver V/8979. 올림픽/올림픽.py
@@ -4,4 +4,8 @@ result = [list(map(int, input().split())) for _ in range(n)]
 result.sort(key = lambda x: (x[1], x[2], x[3]), reverse=True)
 
 idx = [result[i][0] for i in range(n)].index(k)
-print(idx)
+
+for i in range(n):
+    if result[idx][1:] == result[i][1:]:
+        print(i + 1)
+        break
