e0dac4317265800e843404a19dbeb28d04f61281
Youjin <114130942+Youjiiin@users.noreply.github.com>
2024-02-21 15:17:54 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,13 +1,13 @@
 n = int(input())
-stairs = [0] * 301
+step = [0] * 301
 for i in range(1, n + 1):
-    stairs[i] = int(input())
-
-steps = [0] * 301
-steps[1] = stairs[1]
-steps[2] = stairs[1] + stairs[2]
-steps[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3])
+    step[i] = int(input())
+dp = [0] * 301
+dp[1] = step[1]
+dp[2] = step[1] + step[2]
+dp[3] = max(step[1] + step[3], step[2] + step[3])
 
 for i in range(4, n + 1):
-    steps[i] = max(steps[i - 3] + stairs[i - 1] + stairs[i], steps[i - 2] + stairs[i])
-print(steps[n])
+    dp[i] = max(dp[i - 3] + step[i - 1] + step[i], dp[i - 2] + step[i])
+
+print(dp[n])
