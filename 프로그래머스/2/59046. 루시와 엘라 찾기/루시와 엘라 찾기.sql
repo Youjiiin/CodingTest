3dcd1db7e4d801776d60ea2c586314b2a33fc423
Youjin <114130942+Youjiiin@users.noreply.github.com>
2024-02-22 21:56:26 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,4 +1,4 @@
 -- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
-FROM ANIMAL_INS
-WHERE NAME = 'Lucy' OR NAME = 'Ella' OR NAME = 'Pickle' OR NAME = 'Rogan' OR NAME = 'Sabrina' OR NAME = 'Mitty'
+FROM ANIMAL_INS 
+WHERE NAME = 'Lucy' OR  NAME = 'Ella' OR  NAME = 'Pickle' OR  NAME = 'Rogan' OR  NAME = 'Sabrina' OR NAME = 'Mitty'
