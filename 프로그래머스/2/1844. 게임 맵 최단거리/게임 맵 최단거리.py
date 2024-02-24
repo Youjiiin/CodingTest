e1b0393e443bbf0b0b242ea7a4aa9b1e55394d53
Youjin <114130942+Youjiiin@users.noreply.github.com>
2024-02-24 12:40:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.72 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -2,31 +2,30 @@ from collections import deque
 def bfs(x, y, maps):
     n = len(maps)
     m = len(maps[0])
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
-    
     q = deque()
     q.append((x, y))
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
     
     while q:
         x, y = q.popleft()
+        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
             if maps[nx][ny] == 0:
                 continue
-
+                
             if maps[nx][ny] == 1:
                 maps[nx][ny] = maps[x][y] + 1
                 q.append((nx, ny))
-    return maps[n - 1][m - 1]
-    
+    return maps[n-1][m-1]
+
 def solution(maps):
     answer = bfs(0, 0, maps)
-    
     if answer == 1:
         return -1
     else:
