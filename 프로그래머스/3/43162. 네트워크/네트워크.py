90d55b5eb6b4b8aeb9f65d6cf8417e404f02b98e
Youjin <114130942+Youjiiin@users.noreply.github.com>
2024-03-01 23:09:53 +0900

    [level 3] Title: 네트워크, Time: 0.22 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,15 +1,24 @@
-def dfs(v, visited, n, computers):
+from collections import deque
+
+def bfs(v, computers, visited, n):
+    q = deque()
+    q.append(v)
     visited[v] = True
-    for i in range(n):
-        if computers[v][i] == 1 and visited[i] == False:
-            dfs(i, visited, n, computers)
-            
+    
+    while q:
+        v = q.popleft()
+        
+        for i in range(n):
+            if computers[v][i] == 1 and visited[i] == False:
+                visited[i] = True
+                q.append(i)
+
 def solution(n, computers):
     answer = 0
     visited = [False] * n
     for i in range(n):
-        if not visited[i]:
-            dfs(i, visited, n, computers)
+        if visited[i] == False:
+            bfs(i, computers, visited, n)
             answer += 1
                 
     return answer
