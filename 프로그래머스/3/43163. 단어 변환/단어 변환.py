df6e9695b70f4fa1f923afa267d231961ca58afa
Youjin <114130942+Youjiiin@users.noreply.github.com>
2024-03-01 23:52:35 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,24 +1,30 @@
 from collections import deque
 def solution(begin, target, words):
     answer = 0
+    
     if target not in words:
         return 0
+    
     q = deque()
     q.append((begin, 0))
+    visited = [False] * len(words)
     
     while q:
-        w, step = q.popleft()
+        w, count = q.popleft()
         
         if w == target:
-            answer = step
+            answer = count
             break
         
-        for i in words:
-            count = 0
-            for j in range(len(w)):
-                if w[j] != i[j]:
-                    count += 1
-            if count == 1:
-                q.append((i, step + 1))
-    
+        for i in range(len(words)):
+            dif = 0
+            if not visited[i]:
+                for j in range(len(w)):
+                    if words[i][j] != w[j]:
+                        dif += 1
+                    
+                if dif == 1:
+                    visited[i] = True
+                    q.append((words[i], count + 1))
+        
     return answer
