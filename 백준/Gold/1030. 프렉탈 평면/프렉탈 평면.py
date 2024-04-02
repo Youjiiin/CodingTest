fe5b92602456fb728b5fe735e214f450e52b85b0
Youjin <114130942+Youjiiin@users.noreply.github.com>
2024-04-02 15:10:20 +0900

    [Gold III] Title: 프렉탈 평면, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1030. 프렉탈 평면/프렉탈 평면.py
+++ 백준/Gold/1030. 프렉탈 평면/프렉탈 평면.py
@@ -1,21 +1,29 @@
-import sys
-input = sys.stdin.readline
+def paint(x, y, size, n, k):
+    # size가 1보다 작거나 같을 때, 전체 영역이 검은색 또는 흰색
+    if size <= 1:
+        return 0  # 흰색으로 처리
+    
+    # 현재 영역의 한 변의 길이
+    newSize = size // n
+    
+    # 중앙 검은색 영역을 확인
+    blackStart = (n - k) // 2 * newSize
+    blackEnd = ((n + k) // 2) * newSize
+    
+    # 현재 위치가 중앙 검은색 영역에 있는지 확인
+    if blackStart <= x < blackEnd and blackStart <= y < blackEnd:
+        return 1  # 검은색
+    else:
+        # 현재 위치를 새 영역에 맞게 조정
+        newX = x % newSize
+        newY = y % newSize
+        return paint(newX, newY, newSize, n, k)
 
-def check_black(l, point):
-    x,y = point # 현재 탐색중인 좌표
-    center = l//N # 검정색 칸의 범위
-    if l == 1:
-        return 0
-    if center * (N-K)//2 <= x < center * (N+K)//2 and center * (N-K)//2 <= y < center * (N+K)//2:
-        return 1
-    x %= center
-    y %= center
-    return check_black(l//N, [x,y])
-s,N,K,R1,R2,C1,C2 = map(int,input().split())
-l = N**s # 한 변의 길이
+# 입력 처리
+s, n, k, r1, r2, c1, c2 = map(int, input().split())
+side = n ** s
 
-
-for i in range(R1, R2+1):
-    for j in range(C1, C2+1):
-        print(check_black(l,[i,j]), end="")
+for i in range(r1, r2 + 1):
+    for j in range(c1, c2 + 1):
+        print(paint(i, j, side, n, k), end="")
     print()
