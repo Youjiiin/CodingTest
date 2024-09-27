668743dab837de9adb19e6a3e713e313fec177d7
Yujin <114130942+Youjiiin@users.noreply.github.com>
2024-09-27 22:15:11 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,19 +1,8 @@
 t = int(input())
-for i in range(t):
+for _ in range(t):
     n = int(input())
-    if n == 0:
-        print('1 0')
-    elif n == 1:
-        print('0 1')
-    else:
-        dp = [[0, 0] for _ in range(n + 1)]
-        dp[0][0] = 1
-        dp[0][1] = 0
-        dp[1][0] = 0
-        dp[1][1] = 1
+    a, b = 1, 0
 
-        for j in range(2, n + 1):
-            dp[j][0] = dp[j - 1][0] + dp[j - 2][0]
-            dp[j][1] = dp[j - 1][1] + dp[j - 2][1]
-
-        print(*dp[n])
+    for i in range(n):
+        a, b = b, a + b
+    print(a, b)
